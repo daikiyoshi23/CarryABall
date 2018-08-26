b0fa4a6fc30319fccb7fd4cab653bd7adbf54ba1
DaiDaiPc\Daiki <21131157dy@tama.ac.jp>
2018-08-26 13:54:38 +0900

    修正（主にスクリプト）

--- Assets/Script/Goal.cs
+++ Assets/Script/Goal.cs
@@ -5,19 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour {
 
+    //ゴールのスピード
     public float speed = 2.0f;
+
+    //ゴールを一定の時間で動かすため
     float time;   
+
     //方向を変えるスイッチ
     public bool change = false;
-    Vector3 TransPos;
+
+    //ゴールした時のエフェクト
+
     GameObject goaleffect;
-    //ゴールした時、パーティクルを呼び出さないためのスイッチ
+    
+    //ゴールした時、もう一度パーティクルを呼び出さないためのスイッチ
     public bool goal;
 
 
     // Use this for initialization
     void Start () {
-        TransPos = transform.position;
         time = 0f;
         goal = false;
 	}
@@ -26,18 +32,25 @@ public class Goal : MonoBehaviour {
 	void Update () {
         time += Time.deltaTime;
 
+        //ゴールを一定時間左側に動かす
         if (change == false )
         {
             transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
+
+            //一定時間経過したら、反対方向に切り返す
             if (time >= 2.0f)
             {
                 change = true;
                 time = 0f;
             }
         }
+        
+        //ゴールを一定時間右側に動かす
         else
         {
             transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+
+            //一定時間経過したら、反対方向に切り返す
             if (time >= 2.0f)
             {
                 change = false;
@@ -46,17 +59,24 @@ public class Goal : MonoBehaviour {
         }
 	}
 
+    //ボールがゴールに入った場合
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Ball" && goal == false)
         {
+            //エフェクトをゴールの場所に呼び出す
             goaleffect = Resources.Load<GameObject>("Effect/magic_ring_02");
             Instantiate(goaleffect, gameObject.transform.position, new Quaternion(45, transform.rotation.y, transform.rotation.z, transform.rotation.w));
+            
+            //エフェクトを2度呼び出さないようtrueにする
             goal = true;
-            Invoke("LoadScene", 3f);
+            
+            //数秒後にクリア画面を呼ぶ
+            Invoke("LoadScene", 2f);
         }    
     }
 
+    //クリア画面を呼ぶ関数
     void LoadScene()
     {
         SceneManager.LoadScene("Clear");
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    //横移動の速さ
     public float speedX;
+
+    //縦移動の速さ
     public float speedY;
+
+    //シーソーを動かすためのキャラクターから下向きに加える力
     public float power;
 
 
@@ -14,17 +19,23 @@ public class PlayerController : MonoBehaviour
 
     Animator anim;
 
+   　//キャラクターが左に向くときの対象物
     public GameObject lefttarget;
+    
+    //キャラクターが右に向くときの対象物
     public GameObject righttarget;
     
+    //左に向かせる対象物の場所
     Transform ltarget;
+
+    //右に向かせる対象物の場所
     Transform rtarget;
     
-
+    //シーソーを影響させるためのオブジェクト
     public GameObject SeeSaw;
 
 
-    private Rigidbody PlayerRigid;
+   
 
 
 
@@ -36,7 +47,7 @@ public class PlayerController : MonoBehaviour
         ltarget = lefttarget.GetComponent<Transform>();
         rtarget = righttarget.GetComponent<Transform>();
         
-        PlayerRigid = this.transform.GetComponent<Rigidbody>();
+        
 
 
     }
@@ -44,48 +55,66 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
-
-
+        //キー移動のため
         float horizontal = Input.GetAxis("Horizontal");
+
+        //キャラクターの現在いる場所
         Transform thistransform = this.transform;
 
+        //キャラクターがワールド空間上で最初にいる場所
         Vector3 worldPos = thistransform.position;
         worldPos.x = 0f;
         worldPos.y = 0f;
         worldPos.z = 0f;
 
 
-
+        //矢印キー左を押したとき
         if (Input.GetKey("left"))
         {
+            //シーソー上で左側に移動させるための式（そのためのSpace.World)
             transform.Translate(horizontal * speedX, 0, 0, Space.World);
+
+            //左キーを押している間は、アニメーションをtrueにする
             anim.SetBool("RunLeft", true);
+
+            //左キーを押している間は、左側にあるターゲットを見るようにして移動
             Vector3 ltargetposition = ltarget.position;
 
+            //高さが変わっても対象物を見失わないようにする
             if (transform.position.y != ltarget.position.y)
             {
                 ltargetposition = new Vector3(ltarget.position.x, transform.position.y, ltarget.position.z);
             }
             Quaternion ltargetRotation = Quaternion.LookRotation(ltargetposition - transform.position);
+
+            //徐々に左に向くようにする
             transform.rotation = Quaternion.Slerp(transform.rotation, ltargetRotation, Time.deltaTime * 2f);
 
         }
+
+        //矢印キー右を押している時
         else if (Input.GetKey("right"))
         {
+            //シーソー上で右側に移動させるための式（そのためのSpace.World)
             transform.Translate(horizontal * speedX, 0, 0, Space.World);
 
+            //右キーを押している間は、アニメーションをtrueにする
             anim.SetBool("RunRight", true);
 
+            //右キーを押している間は、右側にあるターゲットを見るようにして移動
             Vector3 rtargetposition = rtarget.position;
-
+           
+            //高さが変わっても対象物を見失わないようにする
             if (transform.position.y != rtarget.position.y)
             {
                 rtargetposition = new Vector3(rtarget.position.x, transform.position.y, rtarget.position.z);
             }
             Quaternion rtargetRotation = Quaternion.LookRotation(rtargetposition - transform.position);
+            
+            //徐々に右に向くようにする
             transform.rotation = Quaternion.Slerp(transform.rotation, rtargetRotation, Time.deltaTime * 2f);
         }
+        //それ以外はアニメーションをfalseにする
         else
         {
             anim.SetBool("RunLeft", false);
@@ -93,10 +122,10 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-
+    //シーソーに触れている間、キャラクターのいる場所から下向きに力を加える設定
     void OnCollisionStay(Collision collision)
     {
-
+        //キャラクターがシーソータグだった場合のみ、力を加える
         if (collision.gameObject.tag == "SeeSaw")
         {
             collision.gameObject.GetComponent<Rigidbody>().AddForceAtPosition(Vector3.down * power, transform.position, ForceMode.Force);
